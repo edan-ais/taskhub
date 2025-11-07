10480d749194ef55ba873158daa49e4db92a114d
Edan Harr <edan@analyticintelligencesolutions.com>
2025-11-07 00:07:43 -0800

    Update BottomNavigation.tsx

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -27,21 +27,21 @@ export function BottomNavigation() {
                 onClick={() => setActiveTab(tab.id)}
                 className="relative flex flex-col items-center justify-center flex-1 h-full transition-colors"
               >
-                <div className="relative flex flex-col items-center justify-center">
-                  {isActive && (
-                    <motion.div
-                      layoutId="activeTabIndicator"
-                      className="absolute -top-1 left-1/2 -translate-x-1/2 w-8 h-1.5 rounded-full"
-                      style={{ backgroundColor: tab.color }}
-                      transition={{ type: 'spring', bounce: 0.2, duration: 0.6 }}
-                    />
-                  )}
-                  <div
-                    className="transition-colors"
-                    style={{ color: isActive ? tab.color : '#64748B' }}
-                  >
-                    {tab.icon}
-                  </div>
+                {/* Highlight bar perfectly above tab */}
+                {isActive && (
+                  <motion.div
+                    layoutId="activeTabIndicator"
+                    className="absolute top-0 left-1/2 -translate-x-1/2 w-10 h-[3px] rounded-full"
+                    style={{ backgroundColor: tab.color }}
+                    transition={{ type: 'spring', bounce: 0.2, duration: 0.6 }}
+                  />
+                )}
+
+                <div
+                  className="transition-colors"
+                  style={{ color: isActive ? tab.color : '#64748B' }}
+                >
+                  {tab.icon}
                 </div>
                 <span
                   className="text-xs mt-1 font-medium transition-colors"
